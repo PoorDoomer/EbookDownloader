1e731e0c4db8ca8e68648de01c9abdf0fd5d9ef4
PoorDoomer <65148103+PoorDoomer@users.noreply.github.com>
2024-10-14 10:25:58 +0100

    Update env.cs

--- env.cs
+++ env.cs
@@ -6,7 +6,7 @@ namespace EnvironmentVariable
 {
     public static class EnvironmentVariables
     {
-        public static string apiKey = "1fcf43c6bcmsh35f0f00d1feba1bp19e7c7jsneb69bb3999eb";
+        public static string apiKey = "yourAPIKey";
         public static string apiHost = "annas-archive-api.p.rapidapi.com";
         public static string baseUrl = "https://annas-archive-api.p.rapidapi.com";
     }
